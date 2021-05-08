d89f5367e3da52c32fd5580889f14aceb75a9448
Guadalupe Brenes <brenesmesa@gmail.com>
2021-05-07 22:44:41 -0500

    Add Navbar layoit

---
+++ components/Layout/Sidebar/Navbar.js
@@ -0,0 +1,13 @@
+// Import icons
+import WomenIcon from '../../../assets/icons/mujer.svg'
+
+const Navbar = () => {
+  return (
+    <ul>
+      <li><WomenIcon/></li>
+      <li></li>
+    </ul>
+  )
+}
+
+export default Navbar
