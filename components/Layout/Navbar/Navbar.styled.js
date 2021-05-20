8b994f3320b2b71088d8ab177ac6b965f300c754
Guadalupe Brenes <brenesmesa@gmail.com>
2021-05-20 18:31:59 -0500

    Add Navbar styles

--- components/Layout/Navbar/Navbar.styled.js
+++ components/Layout/Navbar/Navbar.styled.js
@@ -1,7 +1,11 @@
 import styled from 'styled-components'
 
 export const Nav = styled.div`
-  height: 150px;
+  display: flex;
+  align-items: center;
+  height: 60px;
+  padding: 0 30px;
+  box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.08);
   .icon {
     width: 50px;
   }
