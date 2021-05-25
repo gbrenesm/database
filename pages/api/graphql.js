f1c233aa7dd1c034d662cfcfe5cbd1895c256a41
Guadalupe Brenes <brenesmesa@gmail.com>
2021-05-24 21:29:30 -0500

    Add ApolloServer

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,2 +1,20 @@
 import { ApolloServer } from 'apollo-server-micro'
 
+import mongoConnect from '../../utils/mongoConnect'
+import typeDefs from '../../graphQL/typeDefs'
+import resolvers from '../../graphQL/resolvers'
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+const handler = mongoConnect(server.createHandler({ path: '/api/graphql' }))
+
+export const config = {
+  api: {
+    bodyParser: false,
+  }
+}
+
+export default handler
