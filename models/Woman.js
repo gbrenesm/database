87e6ef891b9b0e4ec5d3dbe8663651a5da1845e0
Guadalupe Brenes <brenesmesa@gmail.com>
2021-06-01 18:23:42 -0500

    Add Woman model

---
+++ models/Woman.js
@@ -0,0 +1,26 @@
+import { Schema, model, models } from 'mongoose'
+
+const WomanSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: Number,
+  birthday: Date,
+  death: Date,
+  note: String,
+  events: Date,
+  place: String,
+  who: String,
+  what: {
+    type: String,
+    required: true
+  },
+  description: String,
+  records: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Record'
+  }]
+})
+
+export default models.Woman || model('Woman', WomanSchema)
