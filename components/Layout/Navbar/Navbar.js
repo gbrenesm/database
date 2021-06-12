a904a961683a532c0f230c747f8ff9bfb6c29156
Guadalupe Brenes <brenesmesa@gmail.com>
2021-06-12 16:20:41 -0500

    Condition for add woman link

--- components/Layout/Navbar/Navbar.js
+++ components/Layout/Navbar/Navbar.js
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router'
 import Link from 'next/link'
 
 // Import styles
@@ -8,14 +9,18 @@ import WomenIcon from '../../../assets/icons/mujer.svg'
 
 
 const Navbar = () => {
+  const router = useRouter()
+  const path = router.pathname
+  console.log(path)
   return (
     <Nav>
       <div>
         {/* <WomenIcon className="icon"/> */}
       </div>
+      {path !== '/add-woman' &&
       <div>
         <Link href="/add-woman"><a>Agregar mujer</a></Link>
-      </div>
+      </div>}
     </Nav>
   )
 }
